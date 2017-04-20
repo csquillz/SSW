c4db1ade2d75d6757ef202e28ed985e9cbdc4e00
Caroline <carolinesquillante@gmail.com>
2017-04-20 09:15:55 -0400

    more button stuff

--- __init__.py
+++ __init__.py
@@ -14,6 +14,59 @@ def hobby1():
 
     return render_template('hobby1.html')
 
+@app.route('/hobby2')
+
+def hobby2():
+
+    return render_template('hobby2.html')
+
+@app.route('/hobby3')
+
+def hobby3():
+
+    return render_template('hobby3.html')
+
+@app.route('/hobby4')
+
+def hobby4():
+
+    return render_template('hobby4.html')
+
+@app.route('/hobby5')
+
+def hobby5():
+
+    return render_template('hobby5.html')
+
+@app.route('/hobby6')
+
+def hobby6():
+
+    return render_template('hobby6.html')
+
+@app.route('/hobby7')
+
+def hobby7():
+
+    return render_template('hobby7.html')
+
+@app.route('/hobby8')
+
+def hobby8():
+
+    return render_template('hobby8.html')
+
+@app.route('/hobby9')
+
+def hobby9():
+
+    return render_template('hobby9.html')
+
+@app.route('/hobby10')
+
+def hobby10():
+
+    return render_template('hobby10.html')
 
 if __name__ == '__main__':
 
